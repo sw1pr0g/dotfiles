5200760f38afdd5cdbf8995968879a69da2445b4
Alex Poryadin <alex.poryadih@gmail.com>
2024-11-17 11:52:58 +0300

    Add macos settings, delete unnessesary files

---
+++ macos/settings.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo 'Configuring your mac. Hang tight.'
+osascript -e 'tell application "System Preferences" to quit'
+
+# This file contains my macos settings.
+#
+# Some parts are taken from:
+# - https://github.com/sobolevn/dotfiles
+#
+
+# === General ===
+
+# Hide remaining battery time; show percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.battery ShowTime -string "NO"
+
+# Disable dashboard:
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Disable startup noise:
+sudo nvram SystemAudioVolume=%01
+
+# Scrollbars visible when scrolling:
+defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+
+# Maximize windows on double clicking them:
+defaults write -g AppleActionOnDoubleClick 'Maximize'
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+
+# === Dock ===
+
+# Size:
+defaults write com.apple.dock tilesize -int 45
+
+# Show indicator lights for open apps in Dock:
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Dock size and location:
+defaults write com.apple.Dock size-immutable -bool yes
+
+# Show Dock instantly:
+defaults write com.apple.dock autohide-delay -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+
+# === Finder ===
+
+# Keep folders on top when sorting by name:
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Show Finder path bar:
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Do not show status bar in Finder:
+defaults write com.apple.finder ShowStatusBar -bool false
+
+# Show hidden files in Finder:
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show file extensions in Finder:
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show Library folder
+chflags nohidden ~/Library
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Expand the following File Info panes:
+# “General”, “Open with”, and “Sharing & Permissions”
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+	General -bool true \
+	OpenWith -bool true \
+	Privileges -bool true
+
+
+# === Safari ===
+
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+# Improve Safari security
+defaults write com.apple.Safari \
+  com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled \
+  -bool false
+defaults write com.apple.Safari \
+  com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles \
+  -bool false
+
+
+# === Text editing ===
+
+# Disable auto-capitalization:
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+
+# Restarting apps:
+echo 'Restarting apps...'
+killall Finder
+killall Dock
+
+echo 'Done!'
